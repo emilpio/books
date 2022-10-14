eb312c486ec5bfde7e385373a1dc567b47ed03b4
Emil Pio <emilpio@gmail.com>
2022-10-14 17:08:07 +0200

    finished 4 task

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const select = {
   containerOf: {
     booksList: '.books-list',
     image: '.book__image',
+    filters: '.filters',
   },
   templatesOf: {
     booksTemplate: '#template-book',
@@ -43,18 +44,62 @@ function initAction() {
       event.preventDefault();
 
       if (image.classList.contains('favorite')) {
-        image.classList.remove('favorite') && favoriteBooks.splice(bookId);
+        image.classList.remove('favorite');
+        const bookId = image.getAttribute('data-id');
+        const indexOfBookId = favoriteBooks.indexOf(bookId);
+        favoriteBooks.splice(indexOfBookId, 1);
       } else {
         image.classList.add('favorite');
+        const bookId = image.getAttribute('data-id');
+        console.log('bookId', bookId);
+
+        favoriteBooks.push(bookId);
+        console.log('pushed', favoriteBooks);
       }
-
-      const bookId = image.getAttribute('data-id');
-      console.log('bookId', bookId);
-
-      favoriteBooks.push(bookId);
-      console.log('pushed', favoriteBooks);
     });
   }
+
+  const filter = document.querySelector(select.containerOf.filters);
+  console.log(filter);
+  filter.addEventListener('click', function (callback) {
+    const clickedElement = callback.target;
+    console.log(clickedElement.value);
+
+    if (
+      clickedElement.tagName == 'INPUT' &&
+      clickedElement.type == 'checkbox' &&
+      clickedElement.name == 'filter'
+    ) {
+      if (clickedElement.checked) filters.push(clickedElement.value);
+    } else {
+      const indexOfFilter = filters.indexOf(clickedElement.value);
+      filters.splice(indexOfFilter, 1);
+    }
+  });
 }
 
 initAction();
+
+function filterBooks() {
+  for (let book of dataSource.books) {
+    let shouldBeHidden = false;
+
+    const filterOfHiddenBooks = document.querySelector(
+      select.containerOf.image + '[data-id = "' + book.id + '"]'
+    );
+
+    for (const filter of filters) {
+      if (!book.detail[filter]) {
+        shouldBeHidden = true;
+        break;
+      }
+      if (shouldBeHidden == true) {
+        filterOfHiddenBooks.classList.add('hidden');
+      } else {
+        filterOfHiddenBooks.classList.remove('hidden');
+      }
+    }
+  }
+}
+const filters = [];
+filterBooks();
